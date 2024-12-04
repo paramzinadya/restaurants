8e5eecd9ace6a08d92f3c046a8c02f003a839065
Nadya Paramzina <parramzina@mail.ru>
2024-12-04 17:25:48 +0700

    окно входа

---
+++ restaurants/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace restaurants
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(348, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "войти";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxLogin.Location = new System.Drawing.Point(231, 154);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(320, 26);
+            this.textBoxLogin.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(294, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ВХОД В СИСТЕМУ";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxPassword.Location = new System.Drawing.Point(231, 217);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(320, 26);
+            this.textBoxPassword.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPassword;
+    }
+}
+
---
+++ restaurants/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace restaurants
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBoxLogin.Text = "Введите логин";
+            textBoxLogin.ForeColor = Color.Gray;
+
+            textBoxPassword.Text = "Введите пароль";
+            textBoxPassword.ForeColor = Color.Gray;
+            textBoxPassword.UseSystemPasswordChar = false;
+
+            // Подключение событий (пункт 3)
+            textBoxLogin.Enter += textBoxLogin_Enter;
+            textBoxLogin.Leave += textBoxLogin_Leave;
+            textBoxPassword.Enter += textBoxPassword_Enter;
+            textBoxPassword.Leave += textBoxPassword_Leave;
+        }
+
+        private void textBoxLogin_Enter(object sender, EventArgs e)
+        {
+            if (textBoxLogin.Text == "Введите логин")
+            {
+                textBoxLogin.Text = "";
+                textBoxLogin.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxLogin_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxLogin.Text))
+            {
+                textBoxLogin.Text = "Введите логин";
+                textBoxLogin.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBoxPassword_Enter(object sender, EventArgs e)
+        {
+            if (textBoxPassword.Text == "Введите пароль")
+            {
+                textBoxPassword.Text = "";
+                textBoxPassword.ForeColor = Color.Black;
+                textBoxPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void textBoxPassword_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxPassword.Text))
+            {
+                textBoxPassword.UseSystemPasswordChar = false;
+                textBoxPassword.Text = "Введите пароль";
+                textBoxPassword.ForeColor = Color.Gray;
+            }
+        }
+    }
+}
